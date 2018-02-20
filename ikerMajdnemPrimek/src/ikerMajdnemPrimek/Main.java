79c32e9eb4f16230ec835ede85701b758d0e7b52
tothambrus11 <ambrus@ambrusweb11.hu>
2018-02-20 18:06:30 +0100

    Hozzáadtam a ''Ha prím a számpár egyik tagjai, akkor nem jó a számpár'' esetet.

--- ikerMajdnemPrimek/src/ikerMajdnemPrimek/Main.java
+++ ikerMajdnemPrimek/src/ikerMajdnemPrimek/Main.java
@@ -23,16 +23,31 @@ public class Main {
 		ArrayList<PrimePair> primePairs = new ArrayList<PrimePair>();
 		
 		PrimePair tmpPair;
-		for(int i = 4; i < max; i++) {
+		for(int i = 1; i < max; i++) {
+			//Létrehozok egy számpárt.
 			tmpPair = new PrimePair(i, i+1);
-						
+			
+			//Ha az elsõ szám prím, akkor lépjen tovább a következõre:
+			if(isPrime(tmpPair.num1)) {
+				continue;
+			}
+			//Ha a második szám prím, akkor lépjen tovább kettõt (a következõnek az elsõje se legyen prím)
+			if(isPrime(tmpPair.num2)) {
+				i++;
+				continue;
+			}
+			
 		}
 	}
 	public static boolean isPrime(int number) {
 		//Hogy ne kellejen a páros számokkal osztani.
-		if(number%2==0) {
+		if(number < 2) {
 			return false;
 		}
+		if(number%2==0 && number != 2) {
+			return false;
+		}
+		
 		final int sqrtOfNum = (int) Math.sqrt(number);
 		for(int testDivider = 3; testDivider <= sqrtOfNum; testDivider += 2) {
 			if(number % testDivider == 0) {
