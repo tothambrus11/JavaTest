1d7ce08d4f5802702527ebf9ad738919167d1326
tothambrus11 <ambrus@ambrusweb11.hu>
2018-02-20 21:04:48 +0100

    isSemiPrime() hozzáadva, feladat megoldása

--- ikerMajdnemPrimek/src/ikerMajdnemPrimek/Main.java
+++ ikerMajdnemPrimek/src/ikerMajdnemPrimek/Main.java
@@ -7,63 +7,78 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * @author ambru
+ * @author Tóth Ambrus
  *
  */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner Console = new Scanner(System.in);  // Reading from System.in
 		int max = Integer.parseInt(Console.nextLine());
 		
-		int num1;
-		int num2;
-		
-		ArrayList<PrimePair> primePairs = new ArrayList<PrimePair>();
-		
-		PrimePair tmpPair;
+		//ArrayList<PrimePair> primePairs = new ArrayList<PrimePair>();
+		//PrimePair tmpPair;
 		for(int i = 1; i < max; i++) {
 			//Létrehozok egy számpárt.
-			tmpPair = new PrimePair(i, i+1);
+			//tmpPair = new PrimePair(i, i+1);
 			
-			//Ha az elsõ szám prím, akkor lépjen tovább a következõre:
-			if(isPrime(tmpPair.num1)) {
-				continue;
+			//Megnézem, hogy mind a kettõ félprím-e
+			if(!isSemiPrime(i)) { //Ha az elsõ nem félprím
+				continue; //Továbblép
 			}
-			//Ha a második szám prím, akkor lépjen tovább kettõt (a következõnek az elsõje se legyen prím)
-			if(isPrime(tmpPair.num2)) {
-				i++;
-				continue;
+			else if(!isSemiPrime(i+1)) { //Ha a második nem félprím
+				i++; //Ne legyen a következõ számpárnak az elsõ tagja egy biztosan nem félprím. 
+				continue; //Továbblép
 			}
-			
-			
-			//Megnézem az osztóit a két számnak.
-			
+			else {//Mind2 1/2 prím.
+				//OK
+				System.out.println(i+"\t"+(i+1));
+			}
 		}
 	}
-	
+	public static boolean isSemiPrime(int number) {
+		if(isPrime(number)) {
+			return false;
+		}
+		int[] dividers = getDividers(number);
+		for(int i = 0; i < dividers.length; i++) {
+			//Ha az osztó és a hányados prím, akkor ez egy félprím.
+			if(isPrime(dividers[i]) && isPrime(number/dividers[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
 	public static int[] getDividers(int number) {
-		ArrayList<int> dividers = new ArrayList<int>();
-		final int sqrtOfNum = (int) Math.sqrt(number);
-		for(int testDivider = 2; testDivider < sqrtOfNum; testDivider++) {
+		ArrayList<Integer> dividers = new ArrayList<Integer>();
+		final int max = (int) Math.sqrt(number);
+		for(int testDivider = 2; testDivider <= max; testDivider++) {
 			//Ha osztható akkor berakja
-			if((number / testDivider) % 1 == 0) {
+			if(number % testDivider == 0) {
 				dividers.add(testDivider);
 			}
 		}
+		
+		//List to array
+		int[] temp = new int[dividers.size()];
+		for(int i = 0; i < dividers.size(); i++) {
+			temp[i] = dividers.get(i);
+		}
+		return temp;
 	}
 
 	public static boolean isPrime(int number) {
-		//Hogy ne kellejen a páros számokkal osztani.
+		
 		if(number < 2) {
 			return false;
 		}
+		//Hogy ne kellejen a páros számokkal osztani.
 		if(number%2==0 && number != 2) {
 			return false;
 		}
 		
-		final int sqrtOfNum = (int) Math.sqrt(number);
+		final int sqrtOfNum = (int) Math.sqrt(number); //Hogy ne kelljen minden lépésnél kiszámolni a gyökét
 		for(int testDivider = 3; testDivider <= sqrtOfNum; testDivider += 2) {
 			if(number % testDivider == 0) {
 				return false;
@@ -71,5 +86,4 @@ public class Main {
 		}
 		return true;
 	}
-
 }
